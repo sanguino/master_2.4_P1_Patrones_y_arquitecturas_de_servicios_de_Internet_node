e3c02bc964bf543c4a2ad4e67755dd1af4a24c7b
Miguel G. Sanguino <sanguino@me.com>
2021-02-21 22:01:33 +0100

    implement findAll

--- src/infrastructure/ProductRepository.js
+++ src/infrastructure/ProductRepository.js
@@ -1,5 +1,13 @@
+import {ProductEntity} from "./entities/ProductEntity.js";
+
 export const ProductRepository = ({}) => ({
-  findAll () {
-    return [];
+  async findAll() {
+    const products = await ProductEntity.find();
+    return products.map(product => ({
+        id: product._id,
+        name: product.name,
+        price: product.price
+      })
+    );
   }
 });
