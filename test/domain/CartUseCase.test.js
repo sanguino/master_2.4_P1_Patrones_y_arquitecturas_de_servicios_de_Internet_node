e6b0373aeb88c43350c0a298f8897b0640340b57
Miguel G. Sanguino <sanguino@me.com>
2021-02-22 22:49:13 +0100

    refactor new product test, add update test

--- test/domain/CartUseCase.test.js
+++ test/domain/CartUseCase.test.js
@@ -39,22 +39,46 @@ describe('when add product to cart', async () => {
   const prodId = 2;
   const prodQuantity = 3;
 
-  it('and cart and product exists then repositories are called and cart is returned with new product', async () => {
-    sandbox.stub(cartRepository, 'update').resolves({
-      id: cartId,
-      products: new Map([[{id: prodId, name: 'p1', price: 10}, prodQuantity]]),
-      finalized: false
-    });
-    sandbox.stub(cartRepository, 'findById').resolves({id: cartId, products: new Map(), finalized: false});
-    sandbox.stub(productRepository, 'findById').resolves({id: prodId, name: 'p1', price: 10});
+  it('and cart and product exists but not in cart, then repositories are called and cart is returned with new product and quantity', async () => {
+    const product = {id: prodId, name: 'p1', price: 10};
+    const cart = {id: cartId, products: new Map(), finalized: false};
+    sandbox.stub(cartRepository, 'update')
+    sandbox.stub(cartRepository, 'findById').resolves(cart);
+    sandbox.stub(productRepository, 'findById').resolves(product);
 
     const response = await cartUseCaseTest.addOrUpdateProduct(cartId, prodId, prodQuantity);
 
+    const cartToBeUpdated = cartRepository.update.args[0][0];
+
     expect(cartRepository.findById.calledOnce).to.be.true;
     expect(productRepository.findById.calledOnce).to.be.true;
-    expect(response.finalized).to.equal(false);
-    expect(response.products).to.be.a('map');
-    expect(response.products.size).to.equal(1);
+    expect(cartToBeUpdated.finalized).to.equal(false);
+    expect(cartToBeUpdated.products).to.be.a('map');
+    expect(cartToBeUpdated.products.size).to.equal(1);
+    expect(cartToBeUpdated.products.keys().next().value.id).to.equal(prodId);
+    expect(cartToBeUpdated.products.values().next().value).to.equal(prodQuantity);
+    expect(response).not.to.equal(false);
+  });
+
+  it('and cart and product exists and product already in cart with different quantity, then repositories are called and cart is returned with new product and quantity', async () => {
+    const product = {id: prodId, name: 'p1', price: 10};
+    const cart = {id: cartId, products: new Map([[product, prodQuantity + 1]]), finalized: false};
+    sandbox.stub(cartRepository, 'update')
+    sandbox.stub(cartRepository, 'findById').resolves(cart);
+    sandbox.stub(productRepository, 'findById').resolves(product);
+
+    const response = await cartUseCaseTest.addOrUpdateProduct(cartId, prodId, prodQuantity);
+
+    const cartToBeUpdated = cartRepository.update.args[0][0];
+
+    expect(cartRepository.findById.calledOnce).to.be.true;
+    expect(productRepository.findById.calledOnce).to.be.true;
+    expect(cartToBeUpdated.finalized).to.equal(false);
+    expect(cartToBeUpdated.products).to.be.a('map');
+    expect(cartToBeUpdated.products.size).to.equal(1);
+    expect(cartToBeUpdated.products.keys().next().value.id).to.equal(prodId);
+    expect(cartToBeUpdated.products.values().next().value).to.equal(prodQuantity);
+    expect(response).not.to.equal(false);
   });
 
   it('and cart exist but product not, then repositories are called and false is returned', async () => {
